ac99847ac08304fafcb3fa9c86f6d83df6f0c477
Aris Pratama <aris.pratama@student.unsil.ac.id>
2014-05-04 21:49:01 +0700

    Create TUBE1.php
    Buat interfacenya

---
+++ TUBE1.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>TUGAS BESAR 1</title>
+</head>
+<body bgcolor=#00FF00 text=#000000 align=center>
+<h1 align=center>PARSING WEBPAGES DENGAN XPATH</h1>
+<hr width=100% align=left>
+<table bgcolor=#ffff00 height=80% width=100% border=2>
+<tr>
+<td align="center"><h2>Berita 1</h2></td>
+<td align="center"><h2>Berita 2</h2></td>
+<td align="center"><h2>Anggota Kelompok</h2></td>
+</tr>
